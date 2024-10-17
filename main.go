9a638d8050bbe2bb71a61a3ab5b910bc7d776fc0
Cry-StalL <940755868@qq.com>
2024-10-17 10:41:39 +0800

    add CORS settings; complete the frontend's access to backend resources

--- main.go
+++ main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
 	r := gin.Default()
+
+	// 配置 CORS, 解决跨域引用问题
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // 允许的前端地址
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// 绑定静态资源
 	r.Static("/static/panos", "./static/panos")
 	// 启动服务器
 	r.Run(":8080")
