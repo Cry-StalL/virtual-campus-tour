ef67e7459b137b994a9efa176d4218ec262ec552
Cry-StalL <940755868@qq.com>
2024-10-21 16:42:20 +0800

    modify main.go based on dcy's commit

--- main.go
+++ main.go
@@ -36,15 +36,11 @@ func main() {
 	// 绑定静态资源
 	r.Static("/static/panos", "./static/panos")
 
-	// getNextPanorama接口
+	// 定义getNextPanorama接口
 	r.GET("/api/next-pano", func(c *gin.Context) {
-		// 接收请求，获取所有查询参数
-		params := c.Request.URL.Query()
-
-		//log.Println(params)
-
-		direction := params["direction"][0]
-		current_pano := params["current_pano"][0]
+		// 接收请求，获取查询参数
+		direction := c.Query("direction")
+		current_pano := c.Query("current_pano")
 
 		log.Println(direction)
 		log.Println(current_pano)
@@ -55,14 +51,11 @@ func main() {
 			fmt.Println("解析URL出错:", err)
 			return
 		}
-
 		// 获取路径中的文件名
 		current_pano_filename := path.Base(parsedUrl.Path)
-
 		// 查找下划线和扩展名的位置
 		underscoreIndex := strings.LastIndex(current_pano_filename, "_")
 		dotIndex := strings.LastIndex(current_pano_filename, ".")
-
 		// 提取下划线和扩展名之间的数字部分
 		var current_pano_id int
 		if underscoreIndex != -1 && dotIndex != -1 && underscoreIndex < dotIndex {
@@ -78,10 +71,7 @@ func main() {
 			next_pano_id = current_pano_id - 1
 		}
 
-		//next_pano_url := "http://127.0.0.1:8080/static/panos/pano_" + strconv.Itoa(next_pano_id) + ".jpg"
-
-		// 返回响应
-
+		// 返回响应(下一张图的ID)
 		c.JSON(http.StatusOK, gin.H{
 			"next_pano_id": next_pano_id,
 		})
