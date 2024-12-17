ab4905beae3a1481e1badd56144e53b1876cd005
Cry-StalL <940755868@qq.com>
2024-12-17 16:45:34 +0800

    complete aerial pictures switch

--- src/views/home/aerialViewer.vue
+++ src/views/home/aerialViewer.vue
@@ -1,5 +1,15 @@
 <template>
   <div ref="aerialPanorama" :style="{position: 'absolute', width: '100%', height: '100%', zIndex: 1}"></div>
+
+  <div class="container">
+    <!-- 按钮 -->
+    <div class="switch-aerial-button-group">
+      <el-button type="primary" class="aerial-button" @click="aerialViewer.loadScene('aerial1')">若海湖上空</el-button>
+      <el-button type="primary" class="aerial-button" @click="aerialViewer.loadScene('aerial2')">教学楼上空</el-button>
+      <el-button type="primary" class="aerial-button" @click="aerialViewer.loadScene('aerial3')">荔园上空</el-button>
+      <el-button type="primary" class="aerial-button" @click="aerialViewer.loadScene('aerial4')">榕园上空</el-button>
+    </div>
+  </div>
 </template>
 
 <script >
@@ -35,5 +45,11 @@
 </script>
 
 <style scoped>
-
+.switch-aerial-button-group {
+  position: absolute;
+  bottom: 10px; /* 位于屏幕底部 */
+  left: 50%; /* 水平中心 */
+  transform: translateX(-50%); /* 向左偏移自身宽度的50%实现水平居中 */
+  z-index: 10; /* 确保按钮在全景图之上 */
+}
 </style>
