c413f04db3221fcdc78e3986908049c1c23ac21c
Cry-StalL <940755868@qq.com>
2024-12-18 14:40:50 +0800

    modify GetComment

--- src/views/home/sceneViewer.vue
+++ src/views/home/sceneViewer.vue
@@ -83,7 +83,38 @@
               this.sceneViewer.addScene(configs[i].id, configs[i]);
             }
             this.sceneViewer.loadScene('1-2')
-            window.scenePanoViewer = this.sceneViewer; // TODO: 目前viewer是全局的
+            window.sceneViewer = this.sceneViewer; // TODO: 目前viewer是全局的
+
+            const scene_id = this.sceneViewer.getScene();  // 获取当前场景的ID
+
+            axios.get('http://localhost:8080/api/user/GetComment', {
+              params: {
+                scene_id: scene_id,
+              },
+            })
+            .then(response => {
+              console.log(response.data);
+
+              const comments = response.data.data;  // 获取评论数组
+              const num = response.data.num;        // 当前场景的留言数量
+
+              this.hotspotId = num + 1;    //新建hotspots的id
+
+              if (num > 0) {
+                for (let i = 0; i < num; i++) {
+                  const comment = comments[i];
+
+                  console.log(comment);
+
+                  this.sceneViewer.addHotSpot({
+                    pitch: comment.Pitch,
+                    yaw: comment.Yaw,
+                    type: "info",
+                    text: comment.Message,
+                  });
+                }
+              }
+            });
           } else {
             console.error('scenePanorama reference is not available');
           }
--- src/views/home/streetViewer.vue
+++ src/views/home/streetViewer.vue
@@ -40,36 +40,6 @@
 
             window.streetViewer = this.viewer; // TODO: 目前viewer是全局的
 
-            const scene_id = this.viewer.getScene();  // 获取当前场景的ID
-
-            axios.get('http://localhost:8080/api/user/GetComment', {
-              params: {
-                scene_id: scene_id,
-              },
-            })
-            .then(response => {
-              console.log(response.data);
-
-              const comments = response.data.data;  // 获取评论数组
-              const num = response.data.num;        // 当前场景的留言数量
-
-              this.hotspotId = num + 1;    //新建hotspots的id
-
-              if (num > 0) {
-                for (let i = 0; i < num; i++) {
-                  const comment = comments[i];
-
-                  console.log(comment);
-
-                  this.viewer.addHotSpot({
-                    pitch: comment.Pitch,
-                    yaw: comment.Yaw,
-                    type: "info",
-                    text: comment.Message,
-                  });
-                }
-              }
-            });
           }else{
             console.error('streetPanorama reference is not available');
           }
